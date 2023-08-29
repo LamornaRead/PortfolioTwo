65d7a2f77033b64884deefb31602e438e97f58c8
LamornaRead <130292971+LamornaRead@users.noreply.github.com>
2023-08-29 09:15:51 +0100

    form clean up

--- js/form.js
+++ js/form.js
@@ -21,19 +21,7 @@ const countCharacters = () => {
 
 textArea.addEventListener("input", countCharacters);
 
-// form function vars
-
-
-// const firstName = document.getElementById('fname').value;
-// const lastName = document.getElementById('lname').value;
-// const email = document.getElementById('email').value;
-// const subject = document.getElementById('subject').value;
-// const message = document.getElementById('message');
-// const success = document.getElementById('success');
-// const emailFormat = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-// const messageErr = document.getElementById('messageErr');
-
-
+// form function 
 
 function validateForm() {
     var fields = new Array(); 
@@ -64,7 +52,6 @@ function validateForm() {
         }
     }
 
-
     if (err === 0) {
         success.innerHTML = 'Submit Successful';
         success.style.color = '#24d36e';
@@ -84,83 +71,3 @@ function validateForm() {
 }
 
 
-// kept for reference not in use //////////////////////////////////////////////////////////////////////////////////////////////
-
-
-// function checkMessage() {    
-//     if(message.value.length <= 140) {
-//         console.log('message false');
-//         message.style.boxShadow = '0 0 20px red';
-//         message.style.border = '1px solid red';
-//         messageErr.innerHTML = `Please fill in ${message.name}`;
-//     return false
-//     }
-//     if(message.value.length > 140) {
-//             message.style.boxShadow = '';
-//             message.style.border = '';
-//             messageErr.innerHTML = '';
-//             console.log('message true');
-//     }
-// }
-
-// //form function to check one at a time
-
-// function validateForm() {
-//     //first name
-//     if(firstName.length == 0) {
-//         let fnameError = document.getElementById('firstname-error');
-//         fnameError.innerHTML = 'Please Enter First Name';
-//         return false;
-//     } 
-//     if(firstName.length > 0) {
-//         let fnameError = document.getElementById('firstname-error');
-//         fnameError.innerHTML = '';
-//     }
-//     //last name
-//     if(lastName.length == 0) {
-//         let lnameError = document.getElementById('lastname-error');
-//         lnameError.innerHTML = 'Please Enter Last Name';
-//         return false;
-//     }
-//     if(lastName.length > 0) {
-//         let fnameError = document.getElementById('lastname-error');
-//         fnameError.innerHTML = '';
-//     }
-//     //email
-//     if(!emailFormat.test(email) || email.length == 0) {
-//         let emailError = document.getElementById('email-error');
-//         emailError.innerHTML = 'Please Enter A Valid Email';
-//         return false;
-//     }
-//     if(emailFormat.test(email) || email.length > 0) {
-//         let emailError = document.getElementById('email-error');
-//         emailError.innerHTML = '';
-//     }
-//     //subject
-//     if(subject.length == 0) {
-//         let subjectError = document.getElementById('subject-error');
-//         subjectError.innerHTML = 'Please Enter A Subject';
-//         return false;
-//     }
-//     if(subject.length > 0) {
-//         let subjectError = document.getElementById('subject-error');
-//         subjectError.innerHTML = '';
-//     }
-//     //message
-//     if(message.length <= 140) {
-//         let messageError = document.getElementById('message-error');
-//         messageError.innerHTML = 'Your Message Is Too Short';
-//         return false;
-//     }
-//     if(message.length > 140) {
-//         let messageError = document.getElementById('message-error');
-//         messageError.innerHTML = '';
-//     }
-//     success.innerHTML = 'Submit Successful';
-//     success.style.color = '#24d36e';
-//     document.addEventListener('submit', function(event) {
-//         event.preventDefault();
-//         event.target.reset();
-//     });    
-//     return true;   
-// }
